d6b2f1674335f946ee890c639b6ca998a33fe1b8
Vishnu <12vis7@gmail.com>
2026-02-07 14:04:13 -0600

    solved few problems

--- neetcode-150/arrays-hashing/problems.py
+++ neetcode-150/arrays-hashing/problems.py
@@ -6,14 +6,27 @@ def contains_duplicate(nums):
     Given an integer array nums, return true if any value appears at least twice in the array, 
     and return false if every element is distinct.
     """
-    pass
+    return len(set(nums)) != len(nums)
 
 def valid_anagram(s, t):
     """
     242. Valid Anagram
     Given two strings s and t, return true if t is an anagram of s, and false otherwise.
     """
-    pass
+    # if len(s) != len(t):
+    #     return False
+    # return sorted(s) == sorted(t)
+
+    if len(s) != len(t):
+        return False
+    count = {}
+    for c in s:
+        count[c] = count.get(c, 0) + 1
+    for c in t:
+        count[c] = count.get(c, 0) - 1
+        if count[c] < 0:
+            return False
+    return all(c==0 for c in count.values())
 
 def two_sum(nums, target):
     """
@@ -21,14 +34,28 @@ def two_sum(nums, target):
     Given an array of integers nums and an integer target, return indices of the two numbers 
     such that they add up to target.
     """
-    pass
+    mp = {}
+    for i, num in enumerate(nums):
+        if target-num in mp:
+            return [mp[target-num], i]
+        mp[num] = i
+    return [-1, -1]
 
 def group_anagrams(strs):
     """
     49. Group Anagrams
     Given an array of strings strs, group the anagrams together.
     """
-    pass
+    mp = {}
+
+    for str in strs:
+        s = tuple(sorted(str))
+        if s in mp:
+            mp[s].append(str)
+        else:
+            mp[s] = [str]
+    
+    return [v for v in mp.values()]
 
 def top_k_frequent(nums, k):
     """
